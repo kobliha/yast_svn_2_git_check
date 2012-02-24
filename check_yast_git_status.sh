8816ee68604cbf81cbc0198dff79c15f3ba14cae
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-02-24 15:19:48 +0100

    Enhanced script with some fixes

--- check_yast_git_status.sh
+++ check_yast_git_status.sh
@@ -12,29 +12,67 @@ mkdir -p ${CHECKOUTDIR}
 cd ${CHECKOUTDIR} || exit 1
 BASEDIR=`pwd`
 
+# @param string directory name (on disk)
+# @param string repository name (with .git)
+function clone_from_scratch () {
+    cd ${BASEDIR}
+    rm -rf $1
+    git clone ${REPOS_BASE_URL}$2
+
+    return $?
+}
+
+function pull_from_git () {
+    cd ${BASEDIR}
+    cd $1
+    git pull 2>/dev/null
+
+    return $?
+}
+
+function check_one_repo_param () {
+    CHECKED=`LC_ALL=C echo "$1" | sed 's/[a-zA-Z0-9_\.-]//g'`
+    if [ "${CHECKED}" != "" ]; then
+        echo; echo; echo "Error processing \"$1\""
+        echo "Forbidden characters: ${CHECKED}"
+        exit 1
+    fi
+
+    return 0
+}
+
 for ONE_REPO in ${ALL_REPOS}; do
 	#if [ "${ONE_REPO}" == "yast-add-on.git" ]; then
 
-        cd ${BASEDIR}
+        # security check
+        check_one_repo_param ${ONE_REPO}
 
 	echo "Processing \"${ONE_REPO}\""
 	echo "--------------------------------"
         REPO_DIR=`echo "${ONE_REPO}" | sed 's/\.git//'`
 
+        cd ${BASEDIR}
+
 	if [ -e "${REPO_DIR}" ]; then
-	    cd ${REPO_DIR}
-	    git pull || cd ${BASEDIR} && rm -rf ${REPO_DIR} && git clone ${REPOS_BASE_URL}${ONE_REPO} && cd ${REPO_DIR}
+	    pull_from_git ${REPO_DIR} || clone_from_scratch ${REPO_DIR} ${ONE_REPO}
 	else
-	    git clone ${REPOS_BASE_URL}${ONE_REPO}
-	    cd ${REPO_DIR}
+	    clone_from_scratch ${REPO_DIR} ${ONE_REPO}
 	fi
 
-        NR_OF_BRANCHES=`git branch -a | wc -l`
-        NR_OF_TAGS=`git tag | wc -l`
+        cd ${BASEDIR}/${REPO_DIR}
 
-        echo "- There are ${NR_OF_BRANCHES} branches in ${REPO_DIR}"
-        echo "- There are ${NR_OF_TAGS} tags in ${REPO_DIR}"
+        echo; echo "Checking ${REPO_DIR}..."
+
+        NR_OF_BRANCHES=`LC_ALL=C git branch -a | wc -l`
+        NR_OF_TAGS=`LC_ALL=C git tag | wc -l`
+        NR_OF_COMMITS=`LC_ALL=C git log | grep ^commit | wc -l`
+
+        echo "- There are ${NR_OF_BRANCHES} branches"
+        echo "- There are ${NR_OF_TAGS} tags"
+        echo "- There are ${NR_OF_COMMITS} commits in the current branch"
 
         echo
 	#fi
 done
+
+exit 0
